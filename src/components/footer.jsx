827fbaaad702bc00bd24c22f18dae3730dffae9c
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2026-03-31 18:02:01 +0200

    link fix footer
    fixed footer link not working code issue resolved

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ function footer() {
     return(
         <>
             <footer className="text-center text-[#25343F] p-4 bg-[#BFC9D1]">
-                <a className="hover:text-[#FF9B51]" href="Github: https://github.com/OniWithTheHoodie">Link to my Github</a>
+                <a href="https://github.com/OniWithTheHoodie" className="hover:text-[#FF9B51]">Link to my Github</a>
                 <p>© 2026 Lesley Reynolds</p> 
             </footer>
         </>
