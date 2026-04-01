15c0e7ebbfee0092ab262a5de9c80b072e1ccc06
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2026-04-01 08:04:09 +0200

    carousel
    added extra content to the carousel/image

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -9,81 +9,103 @@ function projects() {
                         <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-2">2</a>
                         <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-3">3</a>
                         <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-4">4</a>
+                        <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-4">5</a>
                     </div>
 
                     <div className="flex overflow-x-auto snap-x scroll-smooth [-webkit-overflow-scrolling:touch] slides-custom">
                         <div id="slide-1" className="flex flex-col slide">
-<a href="https://leapforce-cta-section-two-blocks.vercel.app/" className="relative block group overflow-hidden max-w-72">
+                        <a href="https://leapforce-cta-section-two-blocks.vercel.app/" className="relative block group overflow-hidden max-w-72">
 
-  <img
-    className="transition-transform duration-300 group-hover:scale-110"
-    src="./images/LeapForce.png"
-    alt="Leapforce"
-  />
+                        <img
+                          className="transition-transform duration-300 group-hover:scale-110"
+                          src="./images/LeapForce.png"
+                          alt="Leapforce"
+                        />
 
-  <div className="absolute inset-0 bg-black/60 opacity-0 
-                  group-hover:opacity-100 transition 
-                  flex items-center justify-center text-white text-lg font-semibold">
-    View Project
-  </div>
+                          <div className="absolute inset-0 bg-black/60 opacity-0 
+                                          group-hover:opacity-100 transition 
+                                          flex items-center justify-center text-white text-lg font-semibold">
+                            View Project
+                          </div>
 
-</a>
-                    <p>LeapForce</p>
-                    </div>
-<div id="slide-2" className="flex flex-col slide">
-  <a href="https://pleasurable-ui.onrender.com/" className="relative block group overflow-hidden max-w-72">
-    
-    <img
-      className="max-w-72 max-h-100 transition-transform duration-300 group-hover:scale-110"
-      src="./images/Funda.png"
-      alt="Funda project"
-    />
+                        </a>
+                        <p>LeapForce</p>
+                        </div>
 
-    <div className="absolute inset-0 bg-black/60 opacity-0 
-                    group-hover:opacity-100 transition 
-                    flex items-center justify-center text-white text-lg font-semibold">
-      View Project
-    </div>
+                      <div id="slide-2" className="flex flex-col slide">
+                      <a href="https://pleasurable-ui.onrender.com/" className="relative block group overflow-hidden max-w-72">
+                        
+                      <img
+                        className="max-w-72 max-h-100 transition-transform duration-300 group-hover:scale-110"
+                        src="./images/Funda.png"
+                        alt="Funda project"
+                      />
 
-  </a>
-  <p>Funda</p>
-</div>
-<div id="slide-3" className="flex flex-col slide">
-  <a href="https://mediahuis.agency.fdnd.nl/" className="relative block group overflow-hidden max-w-72">
-    
-    <img
-      className="max-w-72 max-h-100 transition-transform duration-300 group-hover:scale-110"
-      src="./images/Mediahuis.png"
-      alt="Mediahuis project"
-    />
+                      <div className="absolute inset-0 bg-black/60 opacity-0 
+                                        group-hover:opacity-100 transition 
+                                        flex items-center justify-center text-white text-lg font-semibold">
+                        View Project
+                      </div>
 
-    <div className="absolute inset-0 bg-black/60 opacity-0 
-                    group-hover:opacity-100 transition 
-                    flex items-center justify-center text-white text-lg font-semibold">
-      View Project
-    </div>
+                      </a>
+                      <p>Funda</p>
+                      </div>
 
-  </a>
-  <p>Mediahuis</p>
-</div>
-<div id="slide-4" className="flex flex-col slide">
-  <a href="https://proof-of-concept-kaoe.onrender.com/" className="relative block group overflow-hidden max-w-72">
-    
-    <img
-      className="max-w-72 max-h-100 transition-transform duration-300 group-hover:scale-110"
-      src="./images/DeloitteHeadstart.png"
-      alt="Deloitte Headstart project"
-    />
+                      <div id="slide-3" className="flex flex-col slide">
+                        <a href="https://mediahuis.agency.fdnd.nl/" className="relative block group overflow-hidden max-w-72">
+                          
+                          <img
+                            className="max-w-72 max-h-100 transition-transform duration-300 group-hover:scale-110"
+                            src="./images/Mediahuis.png"
+                            alt="Mediahuis project"
+                          />
 
-    <div className="absolute inset-0 bg-black/60 opacity-0 
-                    group-hover:opacity-100 transition 
-                    flex items-center justify-center text-white text-lg font-semibold">
-      View Project
-    </div>
+                          <div className="absolute inset-0 bg-black/60 opacity-0 
+                                          group-hover:opacity-100 transition 
+                                          flex items-center justify-center text-white text-lg font-semibold">
+                            View Project
+                          </div>
+                          </a>
+                          <p>Mediahuis</p>
+                          </div>
 
-  </a>
-  <p>Deloitte Headstart</p>
-</div>
+                          <div id="slide-4" className="flex flex-col slide">
+                            <a href="https://proof-of-concept-kaoe.onrender.com/" className="relative block group overflow-hidden max-w-72">
+                              
+                              <img
+                                className="max-w-72 max-h-100 transition-transform duration-300 group-hover:scale-110"
+                                src="./images/DeloitteHeadstart.png"
+                                alt="Deloitte Headstart project"
+                              />
+
+                              <div className="absolute inset-0 bg-black/60 opacity-0 
+                                              group-hover:opacity-100 transition 
+                                              flex items-center justify-center text-white text-lg font-semibold">
+                                View Project
+                              </div>
+
+                            </a>
+                            <p>Deloitte Headstart</p>
+                          </div>
+
+                          <div id="slide-4" className="flex flex-col slide">
+                            <a href="https://oncollaboration.agency.fdnd.nl/" className="relative block group overflow-hidden max-w-72">
+                              
+                              <img
+                                className="max-w-72 max-h-100 transition-transform duration-300 group-hover:scale-110"
+                                src="./images/Oncollaboration.png"
+                                alt="Deloitte Headstart project"
+                              />
+
+                              <div className="absolute inset-0 bg-black/60 opacity-0 
+                                              group-hover:opacity-100 transition 
+                                              flex items-center justify-center text-white text-lg font-semibold">
+                                View Project
+                              </div>
+
+                            </a>
+                            <p>Oncollaboration</p>
+                          </div>
                     </div>
                     </div>      
                 </section>
