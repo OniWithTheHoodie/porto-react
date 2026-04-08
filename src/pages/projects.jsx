adbfdcefd5deb8fc954d660b84f5e0b709731fa5
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2026-04-08 16:18:14 +0200

    Turned off number navigation carousel
    turned off the anchor links to navigate through the carousel until it's fixed.
    
    it is currently jumping vertically to the contact page as well as horizontally to the intended carousel. but it should only scroll horizontally to the intended carousel why staying in the carousel section.

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,14 +3,15 @@ function projects() {
             <>
                 <section id="my-work" className="p-[1em] text-[#25343F] bg-[#BFC9D1] overflow-hidden ">
                     <div className="max-w-7xl mx-auto">
-                    <h2 className="text-2xl">My projects</h2>
-                    <div className="text-center p-[1em] ">
+                    <h2 className="text-2xl pb-3">My projects</h2>
+                    {/* <div className="text-center p-[1em] ">
                         <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-1">1</a>
                         <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-2">2</a>
                         <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-3">3</a>
                         <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-4">4</a>
-                        <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-4">5</a>
-                    </div>
+                        <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-5">5</a>
+                        <a className="slider active:top-[1px] focus:bg-[#FF9B51]" href="#slide-6">6</a>
+                    </div> */}
 
                     <div className="flex overflow-x-auto snap-x scroll-smooth [-webkit-overflow-scrolling:touch] slides-custom">
                         <div id="slide-1" className="flex flex-col slide">
@@ -88,7 +89,7 @@ function projects() {
                             <p>Deloitte Headstart</p>
                           </div>
 
-                          <div id="slide-4" className="flex flex-col slide">
+                          <div id="slide-5" className="flex flex-col slide">
                             <a href="https://oncollaboration.agency.fdnd.nl/" className="relative block group overflow-hidden max-w-72">
                               
                               <img
@@ -105,6 +106,25 @@ function projects() {
 
                             </a>
                             <p>Oncollaboration</p>
+                          </div>
+
+                          <div id="slide-6" className="flex flex-col slide">
+                            <a href="https://oncollaboration.agency.fdnd.nl/" className="relative block group overflow-hidden max-w-72">
+                              
+                              <img
+                                className="max-w-72 max-h-100 transition-transform duration-300 group-hover:scale-110"
+                                src="./images/Github.png"
+                                alt="Deloitte Headstart project"
+                              />
+
+                              <div className="absolute inset-0 bg-black/60 opacity-0 
+                                              group-hover:opacity-100 transition 
+                                              flex items-center justify-center text-white text-lg font-semibold">
+                                View Project
+                              </div>
+
+                            </a>
+                            <p>GitHub</p>
                           </div>
                     </div>
                     </div>      
